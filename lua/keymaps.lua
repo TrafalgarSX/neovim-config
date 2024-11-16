12eac0dba74f85be043f7738e1c62e464defbd86
guoyawen <trafalgar@trafalgar.localdomain>
2024-11-17 00:46:07 +0800

    修改 keymaps 和 options 的代码组织和一些具体配置

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,33 +1,69 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.keymap.set
+
+--[[
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+]]
+
 -- Hint: see `:h vim.map.set()`
 -- Better window navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
-vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
-vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
-vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize with arrows
 -- delta: 2 lines
-vim.keymap.set("n", "<C-Up>", ":resize -2<CR>", opts)
-vim.keymap.set("n", "<C-Down>", ":resize +2<CR>", opts)
-vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Move text up and down
+keymap("n", "<A-j>", ":m .+1<CR>==", opts)
+keymap("n", "<A-k>", ":m .-2<CR>==", opts)
 
 -- For nvim-tree.lua
 -- default leader key: \
-vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 
 -----------------
 -- Visual mode --
 -----------------
+-- Stay in indent mode
+keymap("v", "<", "<gv^", opts)
+keymap("v", ">", ">gv^", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":m '>+1<CR>gv=gv", opts)
+keymap("x", "K", ":m '<-2<CR>gv=gv", opts)
+keymap("x", "<A-j>", ":m '>+1<CR>gv=gv", opts)
 
 -----------------
 -- Insert mode --
 -----------------
-vim.keymap.set("i", "<C-;>", "<End>", opts)
+keymap("i", "<C-;>", "<End>", opts)
 
 -- For nvim-tree.lua
 -- default leader key: \
-vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 
 -- For nvim-treesitter
 -- 1. Press `gss` to intialize selection. (ss = start selection)
@@ -37,8 +73,8 @@ vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 -- 5. Press `gsd` to decrement selection. (sd = selection decrement)
 
 -- nvim-dap
-vim.keymap.set("n", "<F9>", ":lua require'dap'.toggle_breakpoint()<CR>", opts)
-vim.keymap.set("n", "<F5>", ":lua require'dap'.continue()<CR>", opts)
-vim.keymap.set("n", "<F10>", ":lua require'dap'.step_over()<CR>", opts)
-vim.keymap.set("n", "<F11>", ":lua require'dap'.step_into()<CR>", opts)
-vim.keymap.set("n", "<F12>", ":lua require'dap'.step_out()<CR>", opts)
+keymap("n", "<F9>", ":lua require'dap'.toggle_breakpoint()<CR>", opts)
+keymap("n", "<F5>", ":lua require'dap'.continue()<CR>", opts)
+keymap("n", "<F10>", ":lua require'dap'.step_over()<CR>", opts)
+keymap("n", "<F11>", ":lua require'dap'.step_into()<CR>", opts)
+keymap("n", "<F12>", ":lua require'dap'.step_out()<CR>", opts)
--- lua/options.lua
+++ lua/options.lua
@@ -1,38 +1,56 @@
--- Hint: use `:h <option>` to figure out the meaning if needed
-vim.opt.clipboard = 'unnamedplus' -- use system clipboard
-vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
-vim.opt.mouse = 'a' -- allow the mouse to be used in Nvim
--- 禁用自动注释
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*",
-  command = "set formatoptions-=cro"
-})
+local options = {
+	backup = false, -- creates a backup file
+	clipboard = "unnamedplus", -- allows neovim to access the system clipboard
+	completeopt = { "menu", "menuone", "noselect" }, -- mostly just for cmp
+	mouse = "a", -- allow the mouse to be used in neovim
 
--- Tab
-vim.opt.tabstop = 4 -- number of visual spaces per TAB
-vim.opt.softtabstop = 4 -- number of spacesin tab when editing
-vim.opt.shiftwidth = 4 -- insert 4 spaces on a tab
-vim.opt.expandtab = true -- tabs are spaces, mainly because of python
+	--	cmdheight = 2, -- more space in the neovim command line for displaying messages
+	conceallevel = 0, -- so that `` is visible in markdown files
+	fileencoding = "utf-8", -- the encoding written to a file
+	incsearch = true, -- search as characters are entered
+	hlsearch = true, -- highlight all matches on previous search pattern
+	ignorecase = true, -- ignore case in search patterns
+	smartcase = true, -- but make it case sensitive if an uppercase is entered	pumheight = 10, -- pop up menu height
+	showmode = false, -- we don't need to see things like -- INSERT -- anymore
+	showtabline = 2, -- always show tabs
+	smartindent = true, -- make indenting smarter again
+	splitbelow = true, -- force all horizontal splits to go below current window
+	splitright = true, -- force all vertical splits to go to the right of current window
+	swapfile = false, -- creates a swapfile
 
--- UI config
-vim.opt.number = true -- show absolute number
-vim.opt.relativenumber = true -- add numbers to each line on the left side
-vim.opt.cursorline = true -- highlight cursor line underneath the cursor horizontally
-vim.opt.splitbelow = true -- open new vertical split bottom
-vim.opt.splitright = true -- open new horizontal splits right
-vim.opt.termguicolors = true        -- enabl 24-bit RGB color in the TUI
-vim.opt.showmode = false -- we are experienced, wo don't need the "-- INSERT --" mode hint
-vim.o.guifont = "Hack Nerd Font Mono:h12"
+	termguicolors = true, -- set term gui colors (most terminals support this)
+	timeoutlen = 300, -- time to wait for a mapped sequence to complete (in milliseconds)
+	undofile = false, -- enable persistent undo
+	updatetime = 400, -- faster completion (4000ms default)
+	writebackup = true, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+	expandtab = true, -- convert tabs to spaces
+	cursorline = true, -- highlight the current line
+	number = true, -- set numbered lines
+	relativenumber = true, -- set relative numbered lines
+	numberwidth = 4, -- set number column width to 2 {default 4}
 
--- Searching
-vim.opt.incsearch = true -- search as characters are entered
-vim.opt.hlsearch = true -- do not highlight matches
-vim.opt.ignorecase = true -- ignore case in searches by default
-vim.opt.smartcase = true -- but make it case sensitive if an uppercase is entered
+	tabstop = 2, -- insert 2 spaces for a tab
+	softtabstop = 2,
+	shiftwidth = 2, -- the number of spaces inserted for each indentation
+	expandtab = true,
+
+	signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
+	wrap = true, -- display lines as one long line
+	linebreak = true, -- companion to wrap, don't split words
+	scrolloff = 8, -- minimal number of screen lines to keep above and below the cursor
+	sidescrolloff = 8, -- minimal number of screen columns either side of cursor if wrap is `false`
+	guifont = "Hack Nerd Font Mono:h12", -- the font used in graphical neovim applications
+	whichwrap = "bs<>[]", -- which "horizontal" keys are allowed to travel to prev/next line
+}
+
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
 
 -- For nvim-tree
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- For copilot
+-- 禁用自动注释
+vim.opt.formatoptions:remove({ "c", "r", "o" })        -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
