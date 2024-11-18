110d07c52e9da61422066a192ce0b1971cea2078
guoyawen <trafalgar@trafalgar.localdomain>
2024-11-18 16:21:41 +0800

    设置 cmake_kits_path， 这样就可以 select kits 了， 并且 ${kit}也有值了

--- lua/config/cmake.lua
+++ lua/config/cmake.lua
@@ -23,13 +23,13 @@ cmakeTools.setup({
 	cmake_build_directory = function()
 		if osys.iswin32 then
 			-- return "${variant:buildType}"
-			return "${variant:buildType}"
+			return "${kit}/${variant:buildType}"
 		end
-		return "${variant:buildType}"
+		return "${kit}/${variant:buildType}"
 	end, -- this is used to specify generate directory for cmake, allows macro expansion, can be a string or a function returning the string, relative to cwd.
 	cmake_soft_link_compile_commands = true, -- this will automatically make a soft link from compile commands file to project root dir
 	cmake_compile_commands_from_lsp = false, -- this will automatically set compile commands file location using lsp, to use it, please set `cmake_soft_link_compile_commands` to false
-	cmake_kits_path = nil, -- this is used to specify global cmake kits path, see CMakeKits for detailed usage
+	cmake_kits_path = "/home/trafalgar/.local/share/CMakeTools/cmake-tools-kits.json", -- this is used to specify global cmake kits path, see CMakeKits for detailed usage
 	cmake_variants_message = {
 		short = { show = true }, -- whether to show short message
 		long = { show = true, max_length = 40 }, -- whether to show long message
