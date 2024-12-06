c870aa960d2b0cf92e948571469d2734a171efad
trafalgar <guoyawen98@gmail.com>
2024-12-06 23:01:34 +0800

    1. 添加 neogit 插件

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -434,4 +434,18 @@ require("lazy").setup({
       require("config.flash")
     end
 	},
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua",              -- optional
+      "echasnovski/mini.pick",         -- optional
+    },
+    config = true
+  },
+
 })
