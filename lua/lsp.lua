56c192c7bece97a60fd5ef1c6757aa7cc0d3000d
trafalgar <guoyawen98@gmail.com>
2026-04-19 11:58:15 +0800

    change format.

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,183 +1,185 @@
 -- Note: The order matters: require("mason") -> require("mason-lspconfig") -> require("lspconfig")
 
 require("mason").setup({
-  ui = {
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗",
-    },
-  },
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
 
 require("mason-lspconfig").setup({
-  -- A list of servers to automatically install if they're not already installed.
-  ensure_installed = { "ruff", "lua_ls", "bashls", "clangd", "ts_ls", "jsonls", "marksman" },
+	-- A list of servers to automatically install if they're not already installed.
+	ensure_installed = { "ruff", "lua_ls", "bashls", "clangd", "ts_ls", "jsonls", "marksman" },
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("lsp_buf_conf", { clear = true }),
-  callback = function(event_context)
-    local client = vim.lsp.get_client_by_id(event_context.data.client_id)
-    -- vim.print(client.name, client.server_capabilities)
+	group = vim.api.nvim_create_augroup("lsp_buf_conf", { clear = true }),
+	callback = function(event_context)
+		local client = vim.lsp.get_client_by_id(event_context.data.client_id)
+		-- vim.print(client.name, client.server_capabilities)
 
-    if not client then
-      return
-    end
+		if not client then
+			return
+		end
 
-    local bufnr = event_context.buf
+		local bufnr = event_context.buf
 
-    -- Mappings.
-    local map = function(mode, l, r, opts)
-      opts = opts or {}
-      opts.silent = true
-      opts.buffer = bufnr
-      vim.keymap.set(mode, l, r, opts)
-    end
+		-- Mappings.
+		local map = function(mode, l, r, opts)
+			opts = opts or {}
+			opts.silent = true
+			opts.buffer = bufnr
+			vim.keymap.set(mode, l, r, opts)
+		end
 
-    map("n", "gd", function()
-      vim.lsp.buf.definition {
-        on_list = function(options)
-          -- custom logic to avoid showing multiple definition when you use this style of code:
-          -- `local M.my_fn_name = function() ... end`.
-          -- See also post here: https://www.reddit.com/r/neovim/comments/19cvgtp/any_way_to_remove_redundant_definition_in_lua_file/
+		map("n", "gd", function()
+			vim.lsp.buf.definition({
+				on_list = function(options)
+					-- custom logic to avoid showing multiple definition when you use this style of code:
+					-- `local M.my_fn_name = function() ... end`.
+					-- See also post here: https://www.reddit.com/r/neovim/comments/19cvgtp/any_way_to_remove_redundant_definition_in_lua_file/
 
-          -- vim.print(options.items)
-          local unique_defs = {}
-          local def_loc_hash = {}
+					-- vim.print(options.items)
+					local unique_defs = {}
+					local def_loc_hash = {}
 
-          -- each item in options.items contain the location info for a definition provided by LSP server
-          for _, def_location in pairs(options.items) do
-            -- use filename and line number to uniquelly indentify a definition,
-            -- we do not expect/want multiple definition in single line!
-            local hash_key = def_location.filename .. def_location.lnum
+					-- each item in options.items contain the location info for a definition provided by LSP server
+					for _, def_location in pairs(options.items) do
+						-- use filename and line number to uniquelly indentify a definition,
+						-- we do not expect/want multiple definition in single line!
+						local hash_key = def_location.filename .. def_location.lnum
 
-            if not def_loc_hash[hash_key] then
-              def_loc_hash[hash_key] = true
-              table.insert(unique_defs, def_location)
-            end
-          end
+						if not def_loc_hash[hash_key] then
+							def_loc_hash[hash_key] = true
+							table.insert(unique_defs, def_location)
+						end
+					end
 
-          options.items = unique_defs
+					options.items = unique_defs
 
-          -- set the location list
-          ---@diagnostic disable-next-line: param-type-mismatch
-          vim.fn.setloclist(0, {}, " ", options)
+					-- set the location list
+					---@diagnostic disable-next-line: param-type-mismatch
+					vim.fn.setloclist(0, {}, " ", options)
 
-          -- open the location list when we have more than 1 definitions found,
-          -- otherwise, jump directly to the definition
-          if #options.items > 1 then
-            vim.cmd.lopen()
-          else
-            vim.cmd([[silent! lfirst]])
-          end
-        end,
-      }
-    end, { desc = "go to definition" })
-    map("n", "<C-]>", vim.lsp.buf.definition)
-    map("n", "gD", vim.lsp.buf.declaration, { desc = "go to declaration" })
-    map("n", "gi", vim.lsp.buf.implementation, { desc = "go to implementation" })
-    map("n", "go", vim.lsp.buf.type_definition, { desc = "go to type definition" })
-    map("n", "gr", vim.lsp.buf.references, { desc = "go to references" })
-    map("n", "K", function()
-      vim.lsp.buf.hover { border = "single", max_height = 25, max_width = 120 }
-    end)
-    map("n", "<C-k>", vim.lsp.buf.signature_help)
-    map("n", "<space>rn", vim.lsp.buf.rename, { desc = "varialbe rename" })
-    map("n", "<space>ca", vim.lsp.buf.code_action, { desc = "LSP code action" })
-    map("n", "<space>aw", vim.lsp.buf.add_workspace_folder, { desc = "add workspace folder" })
-    map("n", "<space>rw", vim.lsp.buf.remove_workspace_folder, { desc = "remove workspace folder" })
-    map("n", "<space>bf", vim.lsp.buf.document_symbol, { desc = "show document symbols(buffer funtions)" })
-    map("n", "<space>lw", function()
-      vim.print(vim.lsp.buf.list_workspace_folders())
-    end, { desc = "list workspace folder" })
+					-- open the location list when we have more than 1 definitions found,
+					-- otherwise, jump directly to the definition
+					if #options.items > 1 then
+						vim.cmd.lopen()
+					else
+						vim.cmd([[silent! lfirst]])
+					end
+				end,
+			})
+		end, { desc = "go to definition" })
+		map("n", "<C-]>", vim.lsp.buf.definition)
+		map("n", "gD", vim.lsp.buf.declaration, { desc = "go to declaration" })
+		map("n", "gi", vim.lsp.buf.implementation, { desc = "go to implementation" })
+		map("n", "go", vim.lsp.buf.type_definition, { desc = "go to type definition" })
+		map("n", "gr", vim.lsp.buf.references, { desc = "go to references" })
+		map("n", "K", function()
+			vim.lsp.buf.hover({ border = "single", max_height = 25, max_width = 120 })
+		end)
+		map("n", "<C-k>", vim.lsp.buf.signature_help)
+		map("n", "<space>rn", vim.lsp.buf.rename, { desc = "varialbe rename" })
+		map("n", "<space>ca", vim.lsp.buf.code_action, { desc = "LSP code action" })
+		map("n", "<space>aw", vim.lsp.buf.add_workspace_folder, { desc = "add workspace folder" })
+		map("n", "<space>rw", vim.lsp.buf.remove_workspace_folder, { desc = "remove workspace folder" })
+		map("n", "<space>bf", vim.lsp.buf.document_symbol, { desc = "show document symbols(buffer funtions)" })
+		map("n", "<space>lw", function()
+			vim.print(vim.lsp.buf.list_workspace_folders())
+		end, { desc = "list workspace folder" })
 
-    map("n", "<space>dl", '<cmd> lua vim.diagnostic.setloclist()<CR>', { desc = "diagnostic info list" })
-    map("n", "<space>di", '<cmd> lua vim.diagnostic.open_float()<CR>', { desc = "diagnostic info" })
-    -- Set some key bindings conditional on server capabilities
-    -- Disable ruff hover feature in favor of Pyright
-    if client.name == "ruff" then
-      client.server_capabilities.hoverProvider = false
-    end
+		map("n", "<space>dl", "<cmd> lua vim.diagnostic.setloclist()<CR>", { desc = "diagnostic info list" })
+		map("n", "<space>di", "<cmd> lua vim.diagnostic.open_float()<CR>", { desc = "diagnostic info" })
+		-- Set some key bindings conditional on server capabilities
+		-- Disable ruff hover feature in favor of Pyright
+		if client.name == "ruff" then
+			client.server_capabilities.hoverProvider = false
+		end
 
-    if client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-      -- Enable inlay hints if the server supports it.
-      vim.lsp.inlay_hint.enable(true, { buffer = bufnr })
-      map("n", "<space>ih", function()
-        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-      end, { desc = "Toggle Inlay Hints" })
-    end
+		if client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+			-- Enable inlay hints if the server supports it.
+			vim.lsp.inlay_hint.enable(true, { buffer = bufnr })
+			map("n", "<space>ih", function()
+				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+			end, { desc = "Toggle Inlay Hints" })
+		end
 
-    -- Auto-format ("lint") on save.
-    -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
-    if not client:supports_method('textDocument/willSaveWaitUntil')
-        and client:supports_method('textDocument/formatting') then
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        group = vim.api.nvim_create_augroup('lsp_formatting', { clear = false }),
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr, id = client.id, timeout_ms = 1000 })
-        end,
-      })
-    end
+		-- Auto-format ("lint") on save.
+		-- Usually not needed if server supports "textDocument/willSaveWaitUntil".
+		if
+			not client:supports_method("textDocument/willSaveWaitUntil")
+			and client:supports_method("textDocument/formatting")
+		then
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = vim.api.nvim_create_augroup("lsp_formatting", { clear = false }),
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr, id = client.id, timeout_ms = 1000 })
+				end,
+			})
+		end
 
-    -- Uncomment code below to enable inlay hint from language server, some LSP server supports inlay hint,
-    -- but disable this feature by default, so you may need to enable inlay hint in the LSP server config.
-    -- vim.lsp.inlay_hint.enable(true, {buffer=bufnr})
+		-- Uncomment code below to enable inlay hint from language server, some LSP server supports inlay hint,
+		-- but disable this feature by default, so you may need to enable inlay hint in the LSP server config.
+		-- vim.lsp.inlay_hint.enable(true, {buffer=bufnr})
 
-    -- The blow command will highlight the current variable and its usages in the buffer.
-    if client.server_capabilities.documentHighlightProvider then
-      local gid = vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
-      vim.api.nvim_create_autocmd("CursorHold", {
-        group = gid,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.document_highlight()
-        end,
-      })
+		-- The blow command will highlight the current variable and its usages in the buffer.
+		if client.server_capabilities.documentHighlightProvider then
+			local gid = vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
+			vim.api.nvim_create_autocmd("CursorHold", {
+				group = gid,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.document_highlight()
+				end,
+			})
 
-      vim.api.nvim_create_autocmd("CursorMoved", {
-        group = gid,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.clear_references()
-        end,
-      })
-    end
-  end,
-  nested = true,
-  desc = "Configure buffer keymap and behavior based on LSP",
+			vim.api.nvim_create_autocmd("CursorMoved", {
+				group = gid,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.clear_references()
+				end,
+			})
+		end
+	end,
+	nested = true,
+	desc = "Configure buffer keymap and behavior based on LSP",
 })
 
 local capabilities = require("lsp_utils").get_default_capabilities()
 vim.lsp.config("*", {
-  capabilities = capabilities,
-  flags = {
-    debounce_text_changes = 500,
-  },
+	capabilities = capabilities,
+	flags = {
+		debounce_text_changes = 500,
+	},
 })
 -- Enable lsp servers when they are available
 -- A mapping from lsp server name to the executable name
 local enabled_lsp_servers = {
-  lua_ls = "lua-language-server",
-  bashls = "bash-language-server",
-  ts_ls = "typescript-language-server",
-  clangd = "clangd",
-  cmake = "cmake-language-server",
-  ruff = "ruff",
-  jsonls = "vscode-json-language-server",
-  marksman = "marksman",
+	lua_ls = "lua-language-server",
+	bashls = "bash-language-server",
+	ts_ls = "typescript-language-server",
+	clangd = "clangd",
+	cmake = "cmake-language-server",
+	ruff = "ruff",
+	jsonls = "vscode-json-language-server",
+	marksman = "marksman",
 }
 
 for server_name, lsp_executable in pairs(enabled_lsp_servers) do
-  if vim.fn.executable(lsp_executable) ~= 0 then
-    vim.lsp.enable(server_name)
-  else
-    local msg = string.format(
-      "Executable '%s' for server '%s' not found! Server will not be enabled",
-      lsp_executable,
-      server_name
-    )
-    vim.notify(msg, vim.log.levels.WARN, { title = "Nvim-config" })
-  end
+	if vim.fn.executable(lsp_executable) ~= 0 then
+		vim.lsp.enable(server_name)
+	else
+		local msg = string.format(
+			"Executable '%s' for server '%s' not found! Server will not be enabled",
+			lsp_executable,
+			server_name
+		)
+		vim.notify(msg, vim.log.levels.WARN, { title = "Nvim-config" })
+	end
 end
