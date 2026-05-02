04fc69009b51b959b9970b95548f8b84cf7764ff
trafalgar <guoyawen98@gmail.com>
2026-05-02 17:59:27 +0800

    1. 尝试解决 treesitter 打开 markdown 文件报错的问题。

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -33,219 +33,220 @@ vim.opt.rtp:prepend(lazypath)
 --           Lazy-load on key mapping
 --     opts: The table will be passed to the require(...).setup(opts)
 require("lazy").setup({
-  -- lazy.nvim
-  -- 在这里添加 snacks.nvim 的配置
-  {
-    "folke/snacks.nvim",
-    opts = require("config.snacks"),
-  },
-  -- LSP manager
-  {
-    "mason-org/mason.nvim",
-    event = "VeryLazy",
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    event = "VeryLazy",
-  },
-  {
-    "neovim/nvim-lspconfig",
-  },
-  -- Auto-completion engine
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "lspkind.nvim",
-      "hrsh7th/cmp-nvim-lsp", -- lsp auto-completion
-      "hrsh7th/cmp-buffer", -- buffer auto-completion
-      "hrsh7th/cmp-path",  -- path auto-completion
-      "hrsh7th/cmp-cmdline", -- cmdline auto-completion
-    },
-    config = function()
-      require("config.nvim-cmp")
-    end,
-  },
-  -- Git decorations
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("config.gitsigns")
-    end,
-  },
-  -- Autopairs: [], (), "", '', etc
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = function()
-      require("config.nvim-autopairs")
-    end,
-  },
-  -- Treesitter-integration
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("config.nvim-treesitter")
-    end,
-  },
-  -- Nvim-treesitter text objects
-  {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    config = function()
-      require("config.nvim-treesitter-textobjects")
-    end,
-  },
-  -- Status line
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("config.lualine")
-    end,
-  },
-  -- File explorer
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons", -- optional, for file icons
-    },
-    config = function()
-      require("config.nvim-tree")
-    end,
-  },
-  -- Make surrounding easier
-  -- ------------------------------------------------------------------
-  -- Old text                    Command         New text
-  -- ------------------------------------------------------------------
-  -- surr*ound_words             gziw)           (surround_words)
-  -- *make strings               gz$"            "make strings"
-  -- [delete ar*ound me!]        gzd]            delete around me!
-  -- remove <b>HTML t*ags</b>    gzdt            remove HTML tags
-  -- 'change quot*es'            gzc'"           "change quotes"
-  -- delete(functi*on calls)     gzcf            function calls
-  -- ------------------------------------------------------------------
-  {
-    "kylechui/nvim-surround",
-    version = "^4.0.0", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-  },
-  -- Better terminal integration
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = function()
-      require("config.toggleterm")
-    end,
-  },
-  -- Fuzzy finder
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      { "nvim-lua/plenary.nvim" },
-    },
-    config = function()
-      require("config.telescope")
-    end,
-  },
-  -- Colorscheme
-  {
-    "tanvirtin/monokai.nvim",
-  },
-  {
-    "navarasu/onedark.nvim",
-  },
-  {
-    "sainnhe/sonokai",
-  },
-  "sainnhe/everforest",
-  {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.gruvbox_material_background = "soft"
-      vim.g.gruvbox_material_foreground = "mix"
-      vim.g.gruvbox_material_enable_italic = 1
-      vim.g.gruvbox_material_dim_inactive_windows = 1
-      vim.g.gruvbox_material_better_performance = 1
-      vim.g.gruvbox_material_current_word = "high contrast background"
-    end,
-  },
-  {
-    "folke/tokyonight.nvim",
-    event = "VeryLazy",
-    priority = 1000,
-    opts = {},
-  },
-  {
-    "keaising/im-select.nvim",
-    config = function()
-      require("im_select").setup({})
-    end,
-  },
-  {
-    "folke/trouble.nvim",
-    branch = "main",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    keys = {
-      {
-        "<leader>dt",
-        "<cmd>Trouble diagnostics toggle<cr>",
-        desc = "Diagnostics (Trouble)",
-      },
-      {
-        "<leader>df",
-        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-        desc = "Buffer Diagnostics (Trouble)",
-      },
-      {
-        "<leader>cs",
-        "<cmd>Trouble symbols toggle focus=false<cr>",
-        desc = "Symbols (Trouble)",
-      },
-      {
-        "<leader>cl",
-        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-        desc = "LSP Definitions / references / ... (Trouble)",
-      },
-      {
-        "<leader>ll",
-        "<cmd>Trouble loclist toggle<cr>",
-        desc = "Location List (Trouble)",
-      },
-      {
-        "<leader>ql",
-        "<cmd>Trouble qflist toggle<cr>",
-        desc = "Quickfix List (Trouble)",
-      },
-    },
-    opts = function()
-      require("config.trouble")
-    end,
-  },
-  -- {
-  --   "zbirenbaum/copilot.lua",
-  --   cmd = "Copilot",
-  --   event = "InsertEnter",
-  --   config = function()
-  --     require("config.copilot")
-  --   end,
-  -- },
-  -- {
-  --   "zbirenbaum/copilot-cmp",
-  --   config = function()
-  --     require("copilot_cmp").setup()
-  --   end,
-  -- },
-  -- cmake-tools like vscode
-  {
-    "Civitasv/cmake-tools.nvim",
-    event = "BufRead CMakeLists.txt",
-    config = function()
-      require("config.cmake")
-    end,
-  },
+	-- lazy.nvim
+	-- 在这里添加 snacks.nvim 的配置
+	{
+		"folke/snacks.nvim",
+		opts = require("config.snacks"),
+	},
+	-- LSP manager
+	{
+		"mason-org/mason.nvim",
+		event = "VeryLazy",
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		event = "VeryLazy",
+	},
+	{
+		"neovim/nvim-lspconfig",
+	},
+	-- Auto-completion engine
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"lspkind.nvim",
+			"hrsh7th/cmp-nvim-lsp", -- lsp auto-completion
+			"hrsh7th/cmp-buffer", -- buffer auto-completion
+			"hrsh7th/cmp-path", -- path auto-completion
+			"hrsh7th/cmp-cmdline", -- cmdline auto-completion
+		},
+		config = function()
+			require("config.nvim-cmp")
+		end,
+	},
+	-- Git decorations
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("config.gitsigns")
+		end,
+	},
+	-- Autopairs: [], (), "", '', etc
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("config.nvim-autopairs")
+		end,
+	},
+	-- Treesitter-integration
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		build = ":TSUpdate",
+		config = function()
+			require("config.nvim-treesitter")
+		end,
+	},
+	-- Nvim-treesitter text objects
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("config.nvim-treesitter-textobjects")
+		end,
+	},
+	-- Status line
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("config.lualine")
+		end,
+	},
+	-- File explorer
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons", -- optional, for file icons
+		},
+		config = function()
+			require("config.nvim-tree")
+		end,
+	},
+	-- Make surrounding easier
+	-- ------------------------------------------------------------------
+	-- Old text                    Command         New text
+	-- ------------------------------------------------------------------
+	-- surr*ound_words             gziw)           (surround_words)
+	-- *make strings               gz$"            "make strings"
+	-- [delete ar*ound me!]        gzd]            delete around me!
+	-- remove <b>HTML t*ags</b>    gzdt            remove HTML tags
+	-- 'change quot*es'            gzc'"           "change quotes"
+	-- delete(functi*on calls)     gzcf            function calls
+	-- ------------------------------------------------------------------
+	{
+		"kylechui/nvim-surround",
+		version = "^4.0.0", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+	},
+	-- Better terminal integration
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("config.toggleterm")
+		end,
+	},
+	-- Fuzzy finder
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			{ "nvim-lua/plenary.nvim" },
+		},
+		config = function()
+			require("config.telescope")
+		end,
+	},
+	-- Colorscheme
+	{
+		"tanvirtin/monokai.nvim",
+	},
+	{
+		"navarasu/onedark.nvim",
+	},
+	{
+		"sainnhe/sonokai",
+	},
+	"sainnhe/everforest",
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.gruvbox_material_background = "soft"
+			vim.g.gruvbox_material_foreground = "mix"
+			vim.g.gruvbox_material_enable_italic = 1
+			vim.g.gruvbox_material_dim_inactive_windows = 1
+			vim.g.gruvbox_material_better_performance = 1
+			vim.g.gruvbox_material_current_word = "high contrast background"
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		event = "VeryLazy",
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"keaising/im-select.nvim",
+		config = function()
+			require("im_select").setup({})
+		end,
+	},
+	{
+		"folke/trouble.nvim",
+		branch = "main",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		keys = {
+			{
+				"<leader>dt",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>df",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>ll",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>ql",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+		opts = function()
+			require("config.trouble")
+		end,
+	},
+	-- {
+	--   "zbirenbaum/copilot.lua",
+	--   cmd = "Copilot",
+	--   event = "InsertEnter",
+	--   config = function()
+	--     require("config.copilot")
+	--   end,
+	-- },
+	-- {
+	--   "zbirenbaum/copilot-cmp",
+	--   config = function()
+	--     require("copilot_cmp").setup()
+	--   end,
+	-- },
+	-- cmake-tools like vscode
+	{
+		"Civitasv/cmake-tools.nvim",
+		event = "BufRead CMakeLists.txt",
+		config = function()
+			require("config.cmake")
+		end,
+	},
 
 	-- Debugging
 	{
