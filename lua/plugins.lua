757c08d1ca190d5dcaf614aff933fd99b0bceb9f
trafalgar <guoyawen98@gmail.com>
2026-05-24 08:28:20 +0800

    测试 linux 下能否正常使用

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -364,32 +364,6 @@ require("lazy").setup({
 	--   },
 	-- },
 	{
-		"yetone/avante.nvim",
-		event = "VeryLazy",
-		version = false, -- 永远不要将此值设置为 "*"！永远不要！
-		opts = {
-			-- 在此处添加任何选项
-			-- 例如
-			-- provider = "copilot",
-		},
-		-- 如果您想从源代码构建，请执行 `make BUILD_FROM_SOURCE=true`
-		-- build = "make",
-		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false", -- 对于 Windows
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			--- 以下依赖项是可选的，
-			"echasnovski/mini.pick", -- 用于文件选择器提供者 mini.pick
-			"nvim-telescope/telescope.nvim", -- 用于文件选择器提供者 telescope
-			"hrsh7th/nvim-cmp", -- avante 命令和提及的自动完成
-			"ibhagwan/fzf-lua", -- 用于文件选择器提供者 fzf
-			"nvim-tree/nvim-web-devicons", -- 或 echasnovski/mini.icons
-			-- "zbirenbaum/copilot.lua",        -- 用于 providers='copilot'
-		},
-	},
-	{
 		"obsidian-nvim/obsidian.nvim",
 		version = "*", -- recommended, use latest release instead of latest commit
 		ft = { "markdown", "quarto" },
@@ -410,5 +384,61 @@ require("lazy").setup({
 		config = function()
 			require("config.persisted")
 		end,
+	},
+	{
+		"yetone/avante.nvim",
+		-- 如果您想从源代码构建，请执行 `make BUILD_FROM_SOURCE=true`
+		-- ⚠️ 一定要加上这一行配置！！！！！
+		build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource true"
+			or "make",
+		event = "VeryLazy",
+		version = false, -- 永远不要将此值设置为 "*"！永远不要！
+		opts = {
+			-- 在此处添加任何选项
+			-- 例如
+			input = { provider = "dressing" },
+			provider = "deepseek",
+			providers = {
+				deepseek = {
+					__inherited_from = "openai",
+					api_key_name = "DEEPSEEK_API_KEY",
+					endpoint = "https://api.deepseek.com",
+					model = "deepseek-v4-pro",
+					extra_request_body = {
+						max_tokens = 300000,
+						thinking = { type = "enabled" },
+					},
+				},
+			},
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- 以下依赖项是可选的，
+			"echasnovski/mini.pick", -- 用于文件选择器提供者 mini.pick
+			"nvim-telescope/telescope.nvim", -- 用于文件选择器提供者 telescope
+			"hrsh7th/nvim-cmp", -- avante 命令和提及的自动完成
+			"ibhagwan/fzf-lua", -- 用于文件选择器提供者 fzf
+			"nvim-tree/nvim-web-devicons", -- 或 echasnovski/mini.icons
+			"folke/snacks.nvim", -- 确保 snacks 先加载
+			"stevearc/dressing.nvim", -- for input provider dressing
+			{
+				-- 支持图像粘贴
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- 推荐设置
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- Windows 用户必需
+						use_absolute_path = true,
+					},
+				},
+			},
+		},
 	},
 })
