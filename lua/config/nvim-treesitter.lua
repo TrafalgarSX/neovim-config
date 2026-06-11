c8c1620e4bd653efab160ca978c9c0aac724c280
trafalgar <guoyawen98@gmail.com>
2026-06-11 08:02:02 +0800

    1. nvim-treesitter 中的重复代码去除，ensure_install parsers filetype 都可以使用同一个变量
    注意 neovim 中，文件后缀名不代表它的 filetype，比如 .h .cc 的filetype
    实际上是 cpp

--- lua/config/nvim-treesitter.lua
+++ lua/config/nvim-treesitter.lua
@@ -3,73 +3,39 @@ if not is_ok then
 	return
 end
 
+local needed = {
+	"c",
+	"cpp",
+	"cmake",
+	"javascript",
+	"lua",
+	"vim",
+	"yaml",
+	"toml",
+	"rust",
+	"python",
+	"json",
+	"markdown",
+	"markdown_inline",
+	"zsh",
+	"powershell",
+	"nu",
+}
+
 nt.setup({
-	ensure_installed = {
-		"c",
-		"cpp",
-		"cmake",
-		"javascript",
-		"lua",
-		"vim",
-		"yaml",
-		"toml",
-		"rust",
-		"python",
-		"json",
-		"markdown",
-		"markdown_inline",
-		"zsh",
-		"ps1",
-		"nu",
-	},
+	ensure_installed = needed,
 })
 
 -- Deferred: install missing parsers on demand instead of blocking startup
 vim.api.nvim_create_user_command("TSInstallAll", function()
-	local parsers = {
-		"c",
-		"cpp",
-		"cmake",
-		"javascript",
-		"lua",
-		"vim",
-		"yaml",
-		"toml",
-		"rust",
-		"python",
-		"json",
-		"markdown",
-		"markdown_inline",
-		"ps1",
-		"nu",
-		"zsh",
-	}
+	local parsers = needed
 	for _, p in ipairs(parsers) do
 		pcall(vim.cmd, "TSInstall " .. p)
 	end
 end, { desc = "Install all configured treesitter parsers" })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = {
-		"lua",
-		"c",
-		"cpp",
-		"h",
-		"hpp",
-		"cc",
-		"cmake",
-		"javascript",
-		"yaml",
-		"toml",
-		"rust",
-		"python",
-		"json",
-		"markdown",
-		"markdown_inline",
-		"zsh",
-		"ps1",
-		"nu",
-	},
+	pattern = needed,
 	callback = function()
 		-- folds, provided by Neovim
 		vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
